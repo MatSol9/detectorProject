4e0933c21c306ba0d808c26fac2430b614507b66
MatSol9 <mateusz.soltys@protonmail.com>
2021-12-05 19:26:44 +0100

    Allowed stopping/starting cameras via REST. There is an issue with application freezing after camera is stopped, it's probably a problem with the display function

--- src/camera_io/cameraIO.py
+++ src/camera_io/cameraIO.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from queue import Queue
-from typing import Optional, List
+from typing import Optional, List, Dict
 
 import cv2
 
@@ -22,6 +22,9 @@ class CameraReader(mtl.GetParent):
             return None
         return FrameObject(deepcopy(frame), self.index)
 
+    def stop(self):
+        self.cap.release()
+
 
 class CameraDisplay(mtl.SinkParent):
     def __init__(self, window_name: str):
@@ -41,13 +44,14 @@ class CameraDisplay(mtl.SinkParent):
 
 
 class Camera:
-    def __init__(self, index: int, fps: float, data_output: List[Queue]):
+    def __init__(self, index: int, fps: float, data_output: List[Queue], detection_minimums, detection_maximums):
         self.index = index
         self.fps = fps
+        self.status = "INACTIVE"
         data_from_input = [Queue()]
         self.config = Config()
-        self.detection_minimums = self.config.get_detection_minimums(self.index)
-        self.detection_maximums = self.config.get_detection_maximums(self.index)
+        self.detection_minimums = detection_minimums
+        self.detection_maximums = detection_maximums
         self.data_getter = mtl.PeriodicDataGetter(data_from_input, CameraReader(self.index), self.fps)
         self.data_worker_detect = mtl.DataWorker(data_from_input, data_output, mtl.OperationChain()
                                                  .add_operation(
@@ -61,12 +65,54 @@ class Camera:
     def start(self):
         self.data_getter.start()
         self.data_worker_detect.start()
+        self.status = "ACTIVE"
 
     def stop(self):
         self.data_getter.stop()
         self.data_worker_detect.stop()
+        self.status = "INACTIVE"
+
+    def to_dict(self):
+        return {
+            "fps": self.fps,
+            "status": self.status,
+            "detection_minimums": self.detection_minimums,
+            "detection_maximums": self.detection_maximums
+        }
 
     def __str__(self) -> str:
         return " FPS: {}".format(self.fps)
 
 
+class AllCameras:
+    def __init__(self):
+        self.all_cameras: Dict[int, Camera] = {}
+        self.indexes: List[int] = []
+
+    def add_camera(self, index: int, fps: float, data_output: List[Queue], detection_minimums, detection_maximums):
+        self.all_cameras[index] = Camera(index, fps, data_output, detection_minimums, detection_maximums)
+        self.indexes.append(index)
+
+    def start_camera(self, index: int):
+        self.all_cameras[index].start()
+
+    def stop_camera(self, index: int):
+        self.all_cameras[index].stop()
+
+    def start_all_cameras(self):
+        for index in self.indexes:
+            self.start_camera(index)
+
+    def stop_all_cameras(self):
+        for index in self.indexes:
+            self.stop_camera(index)
+
+    def cameras_to_dict(self) -> Dict:
+        result = {}
+        for index in self.indexes:
+            result[index] = self.all_cameras[index].to_dict()
+        return result
+
+    def remove_camera(self, index: int):
+        self.stop_camera(index)
+        self.all_cameras.pop(index)
--- src/main.py
+++ src/main.py
@@ -7,32 +7,45 @@ from data_model.dataModel import Config
 
 
 app = Flask(__name__)
-cameras = []
+cameras = cameraIO.AllCameras()
 
 
 def main():
     config = Config()
     data_from_cameras = []
     for index in config.get_camera_indexes():
+        detection_minimums = config.get_detection_minimums(index)
+        detection_maximums = config.get_detection_maximums(index)
         data_from_cameras.append(Queue())
-        cameras.append(cameraIO.Camera(index, 30, data_from_cameras))
+        cameras.add_camera(index, 30, data_from_cameras, detection_minimums, detection_maximums)
     data_display = mtl.DataSink(data_from_cameras, cameraIO.CameraDisplay("Video"))
-    for camera in cameras:
-        camera.start()
+    # cameras.start_all_cameras()
     data_display.start()
     app.run()
 
 
 def get_cameras():
-    result = {}
-    for camera in cameras:
-        result[camera.index] = camera.__str__()
+    result = cameras.cameras_to_dict()
     return jsonify(result)
 
 
 @app.route('/cameras', methods=['GET'])
-def camerasREST():
+def get_cameras_REST():
     return get_cameras()
+
+
+@app.route('/cameras', methods=['PUT'])
+def stop_start_cameras_REST():
+    index: str = request.args.get("index")
+    active: str = request.args.get("active")
+    if active.__eq__("true"):
+        cameras.start_camera(int(index))
+        return "camera {} turned on".format(index), 200
+    elif active.__eq__("false"):
+        cameras.stop_camera(int(index))
+        return "camera {} turned off".format(index), 200
+    else:
+        return 'bad request!', 400
 
 
 if __name__ == "__main__":
